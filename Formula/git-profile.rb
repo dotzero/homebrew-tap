2e7f2fe385b2dfbd3b4e9a6c28d65a900f3819aa
dotzero <mail@dotzero.ru>
2017-10-17 16:52:05 +0300

    git-profile version v1.1.0

--- Formula/git-profile.rb
+++ Formula/git-profile.rb
@@ -3,10 +3,10 @@ class GitProfile < Formula
   homepage "https://github.com/dotzero/git-profile"
   url "https://github.com/dotzero/git-profile/releases/download/v1.1.0/git-profile_1.1.0_macos_x86_64.tar.gz"
   version "1.1.0"
-  sha256 "064e23e9ffca7402f0e16ae2dd74109587a51e1d878f7ffb9bd27e1d5c77d832"
+  sha256 "c862657da37681baf8d819490100446bcb84d68cb6cf4337f6e76aa67fd39d8b"
 
   def install
-    bin.install "auto-release"
+    bin.install "git-profile"
   end
 
   test do
