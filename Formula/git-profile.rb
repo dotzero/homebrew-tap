f6457509ecfc8a08b041f3646063883700efd131
dotzero <mail@dotzero.ru>
2017-10-19 13:42:23 +0300

    git-profile version v1.2.0

--- Formula/git-profile.rb
+++ Formula/git-profile.rb
@@ -1,9 +1,9 @@
 class GitProfile < Formula
   desc "Allows to add and switch between multiple user profiles in your git repositories"
   homepage "https://github.com/dotzero/git-profile"
-  url "https://github.com/dotzero/git-profile/releases/download/v1.1.0/git-profile_1.1.0_macos_x86_64.tar.gz"
-  version "1.1.0"
-  sha256 "c862657da37681baf8d819490100446bcb84d68cb6cf4337f6e76aa67fd39d8b"
+  url "https://github.com/dotzero/git-profile/releases/download/v1.2.0/git-profile_1.2.0_macos_x86_64.tar.gz"
+  version "1.2.0"
+  sha256 "4e76928e772b9ed0d21cdecbcd953b46e99bf6e68a1d262f9a500bb327b861ea"
 
   def install
     bin.install "git-profile"
